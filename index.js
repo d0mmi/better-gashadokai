66cdc10fbd89ae2fd5f457ab6fdfb51709209a48
Dommi <dommihd@gmail.com>
2023-03-07 17:32:44 +0100

    added more error handling and code cleanup

--- index.js
+++ index.js
@@ -5,26 +5,45 @@ if (!document.getElementById("last-chapter")) {
     last.id = "last-chapter"
     last.innerText = "Last Chapter";
 
-    if (document.getElementsByClassName("page-title").length > 0 && document.getElementsByClassName("page-title")[0].innerHTML == "404") {
+    if (isErrorPage()) {
 
         document.getElementsByClassName("page-header-content")[0].appendChild(last);
-        last.onclick = e => window.location = "/chapter-" + (parseInt(window.location.pathname.match("chapter-([0-9]*)")[1]) - 1);
+        last.onclick = e => window.location = getLastUrl();
 
-    } else {
+    } else if(isChapterPage()) {
 
         document.getElementsByClassName("entry-content-wrap")[0].appendChild(last);
         document.getElementsByClassName("entry-content")[0].after(last);
-        last.onclick = e => window.location = "/chapter-" + (parseInt(window.location.pathname.match("chapter-([0-9]*)")[1]) - 1);
+        last.onclick = e => window.location = getLastUrl();
     }
 }
 
 if (!document.getElementById("next-chapter")) {
-    if (document.getElementsByClassName("page-title").length == 0) {
+    if (isChapterPage()) {
         var next = document.createElement("button");
         next.id = "next-chapter"
         next.innerText = "Next Chapter";
         document.getElementsByClassName("entry-content-wrap")[0].appendChild(next);
         last.after(next);
-        next.onclick = e => window.location = "/chapter-" + (parseInt(window.location.pathname.match("chapter-([0-9]*)")[1]) + 1);
+        next.onclick = e => window.location = getNextUrl();
     }
+}
+
+function isChapterPage() {
+    return window.location.pathname.match("chapter-[0-9]*-") != null;
+}
+function isErrorPage() {
+    return !isChapterPage() && window.location.pathname.match("chapter-[0-9]*") != null;
+}
+
+function getLastUrl() {
+    return "/chapter-" + (getCurrentChapter() - 1);
+}
+
+function getNextUrl() {
+    return "/chapter-" + (getCurrentChapter() + 1);
+}
+
+function getCurrentChapter() {
+    return parseInt(window.location.pathname.match("chapter-([0-9]*)")[1]);
 }
